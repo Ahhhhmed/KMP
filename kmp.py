f737aa38fbcdf58141a8224a1fb1cd77a0002019
Nenad <Ahhhhmed@gmail.com>
2015-02-21 04:20:01 +0100

    Add comments

--- kmp.py
+++ kmp.py
@@ -16,22 +16,27 @@ class KmpMachine:
             return None
         assert (self.__failList == [])
 
+        #first two elements have fixed values
         self.__failList.append(-1)
         if (len(self.__pattern) > 1):
             self.__failList.append(0)
 
         for i in range(2, len(self.__pattern)):
+            #Finding Longest Block (LB), starting from the LB of the last current character
             j = i - 1
 
             while True:
+                #LB is length 0 (does not exist), returning to the beginning of the pattern
                 if j == 0:
                     self.__failList.append(0)
                     break
 
+                #LB for some character can be extended by 1 and that makes LB for the current character
                 if self.__pattern[self.__failList[j]] == self.__pattern[i-1]:
                     self.__failList.append(self.__failList[j] + 1)
                     break
 
+                #LB of LB, next possible LB to be extended
                 j = self.__failList[j]
 
         self.__isCompiled = True
@@ -45,10 +50,12 @@ class KmpMachine:
         match = 0
 
         while index + match < len(text):
+            #Extending partial match
             if text[index + match] == self.__pattern[match]:
                 match += 1
                 if match == len(self.__pattern):
                     return index
+            #Falling back according to the __failList
             else:
                 index = index + match - self.__failList[match]
                 match = max(self.__failList[match], 0)  # if match == 0 is should be 0, not -1
