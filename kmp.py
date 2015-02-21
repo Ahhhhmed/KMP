b35f2b6ad85ec25ea6e327cd3121eb6e8450cfb1
Nenad <Ahhhhmed@gmail.com>
2015-02-21 03:08:55 +0100

    Fix few typos

--- kmp.py
+++ kmp.py
@@ -1,5 +1,6 @@
 __author__ = 'Nenad Vasic'
 
+
 class KmpMachine:
     def __init__(self, pattern):
         if type(pattern) is not str:
@@ -11,14 +12,15 @@ class KmpMachine:
         self.__failList = []
 
     def Compile(self):
-        if self.__isCompiled: return
-        assert(self.__failList == [])
+        if self.__isCompiled:
+            return None
+        assert (self.__failList == [])
 
         self.__failList.append(-1)
-        if (len(self.__pattern)>1):
+        if (len(self.__pattern) > 1):
             self.__failList.append(0)
 
-        for i in range(2,len(self.__pattern)):
+        for i in range(2, len(self.__pattern)):
             j = i - 1
 
             while True:
@@ -31,10 +33,11 @@ class KmpMachine:
                     break
 
                 j = self.__failList[j]
-        self.__isCompiled == True
+
+        self.__isCompiled = True
         assert len(self.__failList) == len(self.__pattern)
 
-    def Search(self,text):
+    def Search(self, text):
         if not self.__isCompiled:
             self.Compile()
 
@@ -42,11 +45,11 @@ class KmpMachine:
         match = 0
 
         while index + match < len(text):
-            if text[index + match] == self.__pattern[match]
+            if text[index + match] == self.__pattern[match]:
                 match += 1
                 if match == len(self.__pattern):
                     return index
             else:
                 index = index + match - self.__failList[match]
-                match = max(self.__failList[match],0) #if match == 0 is should be 0, not -1
+                match = max(self.__failList[match], 0)  # if match == 0 is should be 0, not -1
         return None
