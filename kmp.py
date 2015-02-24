f2f0898a5e2e05d5ffed50e94cdf86ce9198f98e
Nenad <Ahhhhmed@gmail.com>
2015-02-24 04:41:19 +0100

    Change logic to fill state machine more dynamically

--- kmp.py
+++ kmp.py
@@ -18,29 +18,21 @@ class KmpMachine:
 
         #first two elements have fixed values
         self.__failList.append({self.__pattern[0]: 1, 'lb': -1})
-        if (len(self.__pattern) > 1):
-            self.__failList.append(dict(self.__failList[0]))
-            self.__failList[-1]['lb'] = 0
-            self.__failList[-1][self.__pattern[1]] = 2
 
-        for i in range(2, len(self.__pattern)):
+        for i in range(1, len(self.__pattern)):
             #Finding Longest Block (LB), starting from the LB of the last current character
             j = i - 1
 
             while True:
                 #LB is length 0 (does not exist), returning to the beginning of the pattern
                 if j == 0:
-                    self.__failList.append(dict(self.__failList[0]))
-                    self.__failList[-1]['lb'] = 0
-                    self.__failList[-1][self.__pattern[i]] = i+1
+                    self.__failList.append({'lb': 0, self.__pattern[i]: i+1})
                     break
 
                 #LB for some character can be extended by 1 and that makes LB for the current character
                 if self.__pattern[self.__failList[j]['lb']] == self.__pattern[i-1]:
                     failState = self.__failList[j]['lb'] + 1
-                    self.__failList.append(dict(self.__failList[failState]))
-                    self.__failList[-1]['lb'] = failState
-                    self.__failList[-1][self.__pattern[i]] = i+1
+                    self.__failList.append({'lb':failState, self.__pattern[i]: i+1})
                     break
 
                 #LB of LB, next possible LB to be extended
@@ -55,7 +47,13 @@ class KmpMachine:
 
         if letter in self.__failList[state]:
             return self.__failList[state][letter]
-        return 0
+
+        if state == 0:
+            return 0
+
+        failState = self.__nextState(self.__failList[state]['lb'],letter)
+        if failState != 0: self.__failList[state][letter] = failState
+        return failState
 
     def Search(self, text):
         if not self.__isCompiled:
